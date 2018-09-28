588620e9dfaa4af894c5169efd392c1d6b048556
Vadim <vsavdev@gmail.com>
2018-09-28 22:44:38 +0300

    Some main.js refactoring

--- public/main.js
+++ public/main.js
@@ -1,20 +1,24 @@
 function add_to_cart(id)
 {
+    //добавление пицц в корзину
+
     var pizzaID = 'Pizza ID# ' + id;
     var pizzaCount = window.localStorage.getItem(pizzaID);
         pizzaCount = pizzaCount * 1 + 1 //*1 для преобразования в числовой тип
     window.localStorage.setItem(pizzaID, pizzaCount);
-    var cartAmount = 0
-    for (var i = 0; i < localStorage.length; i++)   {
-        cartAmount = cartAmount + localStorage.getItem(localStorage.key(i)) * 1;
-    }
-    document.getElementById('total').innerHTML = cartAmount;
+
+    //выводим количество пицц из корзины (результат выполнения функции cartTotal() в HTML элемент с id total
+
+    document.getElementById('total').innerHTML = cartTotal();
 }
 
-function cartTotal(){
+// считаем общее количество пицц в корзине и выводим в HTML элемент с id total
+
+function cartTotal() {
     var cartAmount = 0
-    for (var i = 0; i < localStorage.length; i++)   {
+    for (var i = 0; i < localStorage.length; i++) {
         cartAmount = cartAmount + localStorage.getItem(localStorage.key(i)) * 1;
+        document.getElementById('total').innerHTML = cartAmount;
+        return cartAmount;
     }
-    document.getElementById('total').innerHTML = cartAmount;
 }
